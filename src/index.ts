9d434735d4c2c1ca43c1fd9109b1ec807938dc78
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-07-18 11:32:07 -0500

    decoded LoginPacket tokens

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Buffer } from 'node:buffer';
 import process from 'node:process';
+import fastJWT from 'fast-jwt';
+import type { LoginToken } from '../../protocol';
 import {
 	framePackets,
 	getPacketId,
@@ -7,9 +9,12 @@ import {
 	NetworkSettingsPacket,
 	CompressionAlgorithm,
 	LoginPacket,
+	ServerToClientHandshakePacket,
 } from '../../protocol';
 import { Server } from './Server';
 import { getNativeObjectAsJsObject } from './utils';
+
+// Decodes login token from the client
 
 const server = new Server('127.0.0.1', 19_132);
 
@@ -17,6 +22,8 @@ server.on('starting', (server) => {
 	// No way to really tell when the server is open raknet native start
 	// server returns a promise that resolves once the server closes
 	console.log(`Starting server on ${server.host}:${server.port}!`);
+	server.setMaxPlayers(100);
+	server.setOnlinePlayers(13);
 });
 
 server.on('packet', ({ bin, id }, client) => {
@@ -34,6 +41,8 @@ server.on('packet', ({ bin, id }, client) => {
 		case LoginPacket.id(): {
 			const data = LoginPacket.deserialize(bin);
 
+			decodeLoginToken(data.tokens);
+
 			console.log('Recieved Login Packet from', client.guid);
 			console.log(getNativeObjectAsJsObject(data));
 
@@ -44,6 +53,34 @@ server.on('packet', ({ bin, id }, client) => {
 			return console.log(`Unhandled packet:`, id.toString(16));
 	}
 });
+
+interface LoginTokenData {
+	clientData: any;
+	displayName: string;
+	identityPublicKey: string;
+	uuid: string;
+	xuid: string;
+}
+
+function decodeLoginToken(token: LoginToken): LoginTokenData | undefined {
+	const decode = fastJWT.createDecoder();
+	const decodedJWT = decode(token.client);
+	const chainData = JSON.parse(token.identity);
+
+	for (const chain of chainData.chain) {
+		const decodedChain = decode(chain);
+
+		if (decodedChain.extraData) {
+			return {
+				displayName: decodedChain.extraData.displayName,
+				identityPublicKey: decodedChain.identityPublicKey,
+				uuid: decodedChain.extraData.identity,
+				xuid: decodedChain.extraData.XUID,
+				clientData: decodedJWT,
+			};
+		}
+	}
+}
 
 server
 	.start()
