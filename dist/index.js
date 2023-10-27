fe81aa3fb7cb37e17ac1a5395f6ba10ad8fc38b3
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-10-26 21:33:16 -0500

    remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Serenity_1 = require("./Serenity");
-const server = new Serenity_1.Serenity('127.0.0.1', 19132);
-server.start();
