0d632260d66f02f94d3483a0a8ec804d1feebfda
DKMtomy <102959198+DKMtomy@users.noreply.github.com>
2024-01-14 18:29:33 +0100

    fully added the text packet

--- packages/serenity/src/network/handlers/Text.ts
+++ packages/serenity/src/network/handlers/Text.ts
@@ -1,4 +1,4 @@
-import { DisconnectReason, type Text } from '@serenityjs/bedrock-protocol';
+import { DisconnectReason, Text } from '@serenityjs/bedrock-protocol';
 import type { NetworkSession } from '../Session';
 import { NetworkHandler } from './NetworkHandler';
 
@@ -11,7 +11,17 @@ class TextHandler extends NetworkHandler {
 		// Disconnect the player if they are null or undefined.
 		if (!player) return session.disconnect('Failed to get player instance.', DisconnectReason.MissingClient);
 
-		console.log(packet);
+		// Send the message to the player.
+		const textPacket = new Text();
+		textPacket.type = packet.type;
+		textPacket.needsTranslation = packet.needsTranslation;
+		textPacket.source = packet.source;
+		textPacket.message = packet.message;
+		textPacket.parameters = packet.parameters;
+		textPacket.xuid = packet.xuid;
+		textPacket.platformChatId = packet.platformChatId;
+
+		await session.send(textPacket);
 	}
 }
 
