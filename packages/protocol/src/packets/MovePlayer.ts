12cff7b7bc37646a6608a1f3293353ad50a27f6c
PMK744 <paytonmk744@outlook.com>
2024-01-15 14:55:36 -0600

    updated MovePlayer packet

--- packages/protocol/src/packets/MovePlayer.ts
+++ packages/protocol/src/packets/MovePlayer.ts
@@ -1,4 +1,4 @@
-import { VarInt, Float32, Endianness, Uint8, Bool, VarLong } from '@serenityjs/binarystream';
+import { Float32, Endianness, Uint8, Bool, VarLong } from '@serenityjs/binarystream';
 import { Packet, Serialize } from '@serenityjs/raknet-protocol';
 import { DataPacket } from '../DataPacket';
 import { MoveMode, Packet as PacketId } from '../enums';
@@ -7,14 +7,14 @@ import { Vec3f, Vector3f, TeleportCause } from '../types';
 
 @Packet(PacketId.MovePlayer)
 class MovePlayer extends DataPacket {
-	@Serialize(VarInt) public runtimeId!: bigint;
+	@Serialize(VarLong) public runtimeId!: bigint;
 	@Serialize(Vector3f) public position!: Vec3f;
 	@Serialize(Float32, Endianness.Little) public pitch!: number;
 	@Serialize(Float32, Endianness.Little) public yaw!: number;
 	@Serialize(Float32, Endianness.Little) public headYaw!: number;
 	@Serialize(Uint8) public mode!: MoveMode;
 	@Serialize(Bool) public onGround!: boolean;
-	@Serialize(VarInt) public riddenRuntimeId!: bigint;
+	@Serialize(VarLong) public riddenRuntimeId!: bigint;
 	@Serialize(TeleportCause) public cause!: TeleportCauseEntry | null;
 	@Serialize(VarLong) public tick!: bigint;
 }
