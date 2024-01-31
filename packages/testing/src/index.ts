1625a061fc5fa92f3f867e3dd9e5c53b0c0b8515
PMK744 <paytonmk744@outlook.com>
2024-01-31 15:48:19 -0600

    Added BlockPermutation.matches

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,9 +1,4 @@
-// import './mapper';
-
-import { AbilityLayerFlag, Packet } from '@serenityjs/bedrock-protocol';
-import { MessageForm, Serenity } from '@serenityjs/serenity';
-
-// import "./mapping_build";
+import { Serenity } from '@serenityjs/serenity';
 
 const serenity = new Serenity({
 	address: '0.0.0.0',
@@ -11,36 +6,3 @@ const serenity = new Serenity({
 });
 
 serenity.start();
-
-// serenity.on('PlayerJoined', ({ player }) => {
-// 	//
-// });
-
-// serenity.on('PlayerSpawned', ({ player }) => {
-// 	//
-// });
-
-// serenity.on('PlayerLeft', ({ player }) => {
-// 	//
-// });
-
-// serenity.on('PlayerChat', ({ player }) => {
-// 	player.abilities.setAbility(AbilityLayerFlag.MayFly, false);
-// });
-
-// serenity.network.on(Packet.BlockPickRequest, ({ player, packet }) => {
-// 	if (!player) return;
-
-// 	const block = player.world.getBlock(packet.x, packet.y, packet.z);
-
-// 	player.world.setBlock(packet.x, packet.y, packet.z, Air);
-
-// 	const form = new MessageForm(
-// 		'BlockPickRequest',
-// 		`You block picked "${block.getName()}". Would you like to destroy this block?`,
-// 		'Yes',
-// 		'No',
-// 	);
-
-// 	player.sendMessageForm(form, (data) => {}).catch(console.error);
-// });
