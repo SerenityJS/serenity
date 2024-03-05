38530dd67d79197ac82a619a5124115dbc6bded5
SkyderG <skydergamer21@gmail.com>
2024-03-05 16:13:18 -0300

    remove CommandOutput comment

--- packages/protocol/src/packets/CommandOutput.ts
+++ packages/protocol/src/packets/CommandOutput.ts
@@ -3,7 +3,6 @@ import { DataPacket } from '../DataPacket.js';
 import { Packet as PacketId, PlayerStatus } from '../enums/index.js';
 import { CommandOutputData } from '../types/CommandOutputData.js';
 
-// Incomplete serialization
 @Packet(PacketId.CommandOutput)
 class CommandOutput extends DataPacket {
 	@Serialize(CommandOutputData) public originData!: CommandOutputData;
