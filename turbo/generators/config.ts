3cd5d5db3a91db04948b3dec2dd740f41dab2467
Nobu <chat@nobu.sh>
2024-03-08 21:10:31 -0600

    generators

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,3 +1,113 @@
-import { PlopTypes } from "@turbo/gen";
+import { readFileSync, writeFileSync } from "node:fs";
 
-export default function generator(plop: PlopTypes.NodePlopAPI): void {}
+import * as JSON5 from "json5";
+import { PlopTypes } from "@turbo/gen";
+import { sync } from "cross-spawn";
+
+const WorkspaceConfigName = "serenityjs.code-workspace";
+const PackagesRoot = "packages";
+
+function validateProjectName(input: string): string | boolean {
+	const lowerCaseWithDashes = /^[\da-z-]+$/;
+
+	if (!lowerCaseWithDashes.test(input)) {
+		return "Project name must be lowercase and contain only letters, numbers, and dashes";
+	}
+
+	return true;
+}
+
+interface WorkspaceConfigMin {
+	folders: Array<{ path: string; name: string }>;
+}
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+	// Creates a new action in plop that allows running commands.
+	plop.setActionType("runCommand", (answers, config) => {
+		if (
+			!config?.data ||
+			!("command" in config.data) ||
+			typeof config.data.command !== "string"
+		) {
+			throw new Error("No command provided");
+		}
+		const command = plop.renderString(config.data.command, answers);
+		const result = sync(command, { stdio: "inherit" });
+
+		if (result.error) {
+			throw result.error;
+		}
+
+		return command;
+	});
+
+	// Creates a new action in plop that allows updating the workspace config.
+	plop.setActionType("updateWorkspaceConfig", (answers) => {
+		const pathToWorkspaceConfig = plop.renderString(
+			`{{ turbo.paths.root }}/.vscode/${WorkspaceConfigName}`,
+			answers
+		);
+		const workspaceConfig = JSON5.parse(
+			readFileSync(pathToWorkspaceConfig, "utf8")
+		) as WorkspaceConfigMin;
+
+		const name = plop.renderString(`📦 {{ titleCase name }}`, answers);
+		const path = plop.renderString("{{ dashCase name }}", answers);
+
+		workspaceConfig.folders.push({
+			name,
+			path: `../${PackagesRoot}/${path}`
+		});
+
+		// Write the updated config back to the file
+		writeFileSync(
+			pathToWorkspaceConfig,
+			JSON.stringify(workspaceConfig, null, 2)
+		);
+
+		return `Appended ${name} to workspace config`;
+	});
+
+	// Adds a package generator
+	plop.setGenerator("Package", {
+		description: "Creates a new package in the workspace",
+		prompts: [
+			{
+				type: "list",
+				name: "type",
+				message: "What type of package do you want to create?",
+				choices: ["TypeScript"]
+			},
+			{
+				type: "input",
+				name: "name",
+				message: "What is the name of the new package?",
+				validate: validateProjectName
+			}
+		],
+		actions: [
+			{
+				type: "addMany",
+				destination: `{{ turbo.paths.root }}/${PackagesRoot}/{{ dashCase name }}`,
+				base: "templates/{{ lowerCase type }}",
+				templateFiles: "templates/{{ lowerCase type }}/**/*",
+				globOptions: {
+					dot: true
+				},
+				skipIfExists: true
+			},
+			{
+				type: "updateWorkspaceConfig",
+				data: {
+					type: "package"
+				}
+			},
+			{
+				type: "runCommand",
+				data: {
+					command: "yarn install"
+				}
+			}
+		]
+	});
+}
