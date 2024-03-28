8da8f9bac7d5cff933e6469d224285cec39c779d
AnyBananaGAME <61703768+AnyBananaGAME@users.noreply.github.com>
2024-03-28 13:02:15 +0200

    Add scale.ts to meta components

---
+++ packages/world/src/components/entity/meta/scale.ts
@@ -0,0 +1,29 @@
+import { MetadataKey, MetadataType } from "@serenityjs/protocol";
+
+import { Entity } from "../../../entity";
+
+import { EntityMetadataComponent } from "./meta";
+
+class EntityScaleComponent extends EntityMetadataComponent {
+	public readonly flag = false;
+
+	public readonly key = MetadataKey.Scale;
+
+	public readonly type = MetadataType.Float;
+
+	public defaultValue = 1;
+
+	public currentValue = this.defaultValue;
+
+	/**
+	 * Set a custom scale for your entity (Width and Height)
+	 *
+	 * @param entity The entity the component is binded to.
+	 * @returns A new entity always show nametag component.
+	 */
+	public constructor(entity: Entity) {
+		super(entity, "minecraft:scale");
+	}
+}
+
+export { EntityScaleComponent };
