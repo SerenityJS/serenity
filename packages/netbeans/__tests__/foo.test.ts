ec531e74a525c270930e62436d034d37b234ff5e
PMK744 <paytonmk744@outlook.com>
2024-04-09 22:08:18 -0500

    refactor: removed neatbeans

--- packages/netbeans/__tests__/foo.test.ts
+++
@@ -1,9 +0,0 @@
-import { foo } from "..";
-
-describe("foo function", () => {
-	it("should return '@serenityjs/netbeans'", () => {
-		const expectedOutput = "@serenityjs/netbeans";
-		const result = foo();
-		expect(result).toBe(expectedOutput);
-	});
-});
--- packages/netbeans/npm/darwin-x64/README.md
+++
@@ -1,3 +0,0 @@
-# `@serenityjs/netbeans-darwin-x64`
-
-This is the **x86_64-apple-darwin** binary for `@serenityjs/netbeans`
