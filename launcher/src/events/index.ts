c2d9f1d2d99193bcc9e09bf8cb1435b3dc860fb1
PMK744 <paytonmk744@outlook.com>
2024-04-13 23:48:08 -0500

    feat(launcher): added PlayerChat event

--- launcher/src/events/index.ts
+++ launcher/src/events/index.ts
@@ -4,15 +4,21 @@ export * from "./event-signal";
 // Concrete signals
 export * from "./player-joined";
 export * from "./player-spawned";
+export * from "./player-chat";
 
 // Import signals
 import { PlayerJoinedSignal } from "./player-joined";
 import { PlayerSpawnedSignal } from "./player-spawned";
+import { PlayerChatSignal } from "./player-chat";
 
 /**
  * Contains all the event signals.
  */
-const EVENT_SIGNALS = [PlayerJoinedSignal, PlayerSpawnedSignal];
+const EVENT_SIGNALS = [
+	PlayerJoinedSignal,
+	PlayerSpawnedSignal,
+	PlayerChatSignal
+];
 
 // Exports
 export { EVENT_SIGNALS };
--- launcher/src/events/signals.ts
+++ launcher/src/events/signals.ts
@@ -1,9 +1,11 @@
+import type { PlayerChatSignal } from "./player-chat";
 import type { PlayerSpawnedSignal } from "./player-spawned";
 import type { PlayerJoinedSignal } from "./player-joined";
 
 interface EventSignals {
 	PlayerJoined: [PlayerJoinedSignal];
 	PlayerSpawned: [PlayerSpawnedSignal];
+	PlayerChat: [PlayerChatSignal];
 }
 
 export { EventSignals };
