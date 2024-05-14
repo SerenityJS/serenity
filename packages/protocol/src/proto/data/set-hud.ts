5db18228135c4ebea3541086e45ce63304ee63da
SkyderG <skydergamer21@gmail.com>
2024-05-13 22:05:04 -0300

    refactor(protocol): fix imports

--- packages/protocol/src/proto/data/set-hud.ts
+++ packages/protocol/src/proto/data/set-hud.ts
@@ -1,9 +1,8 @@
 import { Proto, Serialize } from "@serenityjs/raknet";
 import { VarInt } from "@serenityjs/binarystream";
 
-import { Packet } from "../../enums";
+import { Packet, HUDVisibility } from "../../enums";
 import { HUDElement } from "../types";
-import { HUDVisibility } from "../../enums/hud-visibility";
 
 import { DataPacket } from "./data-packet";
 
