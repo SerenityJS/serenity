cdcfc1d203e2679f1711fd808b4a6b699c72665c
PMK744 <paytonmk744@outlook.com>
2024-06-11 20:25:45 -0500

    feat: added stop command

---
+++ launcher/src/commands/admin/stop.ts
@@ -0,0 +1,44 @@
+import { Player, type World } from "@serenityjs/world";
+import { PermissionLevel } from "@serenityjs/protocol";
+
+import type { Serenity } from "../../serenity";
+
+const register = (world: World, serenity: Serenity) => {
+	// Register the about command
+	world.commands.register(
+		"stop",
+		"Shuts down the server (console only)",
+		(origin) => {
+			// Check if the origin is a player
+			if (origin instanceof Player) {
+				origin.sendMessage(
+					"§cThis command can only be executed from the console§r"
+				);
+				return;
+			} else {
+				// Stop the server
+				serenity
+					.stop()
+					.then(() => {
+						// eslint-disable-next-line unicorn/no-process-exit
+						process.nextTick(() => process.exit(0));
+
+						return;
+					})
+					.catch(() => {
+						return;
+					});
+
+				return {
+					message: "§aServer is shutting down...§r"
+				};
+			}
+		},
+		{},
+		{
+			permission: PermissionLevel.Operator
+		}
+	);
+};
+
+export default register;
