95f854c9f9fad7fb696111e9cbd35faa1952b847
PMK744 <paytonmk744@outlook.com>
2024-06-15 12:40:05 -0500

    chore(serenity): removed tests

--- packages/serenity/__tests__/name.test.ts
+++
@@ -1,9 +0,0 @@
-import { getName } from "..";
-
-describe("getName function", () => {
-  it("should return '@serenityjs/serenity'", () => {
-    const expectedOutput = "@serenityjs/serenity";
-    const result = getName();
-    expect(result).toBe(expectedOutput);
-  });
-});
