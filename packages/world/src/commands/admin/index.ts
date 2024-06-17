efa35d6751b2003464b35b2eaca0f9b5fb56f6df
Felipe-Devr <felipegamerdev1@gmail.com>
2024-06-16 21:35:24 -0700

    Added Clear and Kick commands

--- packages/world/src/commands/admin/index.ts
+++ packages/world/src/commands/admin/index.ts
@@ -2,8 +2,11 @@ import GAMEMODE from "./gamemode";
 import SETBLOCK from "./setblock";
 import GIVE from "./give";
 import SUMMON from "./summon";
+import CLEAR from "./clear";
+import KICK from "./kick";
+import TIME from "./time";
 
 // Define all admin commands
-const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, SUMMON];
+const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, SUMMON, CLEAR, KICK, TIME];
 
 export { ADMIN_COMMANDS };
---
+++ packages/world/src/commands/admin/kick.ts
@@ -0,0 +1,40 @@
+import { DisconnectPacket, DisconnectReason, PermissionLevel } from "@serenityjs/protocol";
+import type { World } from "../../world";
+import { TargetEnum } from "../enums";
+import { Player } from "../../player";
+import { StringEnum } from "@serenityjs/command";
+
+const register = (world: World) => {
+  world.commands.register(
+    "kick",
+    "Kicks a player from the server",
+    (origin, parameters) => {
+      const targets = parameters.name.result;
+      const kickReason = parameters.reason?.result ?? "Kicked by an operator.";
+
+      if (targets.length < 1) return { message: "No targets matched selector" };
+      const packet = new DisconnectPacket();
+      packet.hideDisconnectScreen = false;
+      packet.message = kickReason;
+      packet.reason = DisconnectReason.Kicked;
+
+      for (const target of targets) {
+        if (!(target instanceof Player)) continue;
+        if (origin instanceof Player && origin.xuid == target.xuid) continue;
+        target.session.send(packet);
+      }
+      return {
+        message: `Kicked ${targets.map((target) => (target as Player).username).join(", ")} from the game`,
+      };
+    },
+    {
+      name: TargetEnum,
+      reason: [StringEnum, true],
+    },
+    {
+      permission: PermissionLevel.Operator,
+    }
+  );
+};
+
+export default register;
