d97bfd8da5930531143d15be075eaa4befee96ea
Felipe-Devr <felipegamerdev1@gmail.com>
2024-06-16 22:00:42 -0700

    Revert "Added Clear and Kick commands"
    This reverts commit efa35d6751b2003464b35b2eaca0f9b5fb56f6df.

--- packages/world/src/commands/admin/index.ts
+++ packages/world/src/commands/admin/index.ts
@@ -2,11 +2,8 @@ import GAMEMODE from "./gamemode";
 import SETBLOCK from "./setblock";
 import GIVE from "./give";
 import SUMMON from "./summon";
-import CLEAR from "./clear";
-import KICK from "./kick";
-import TIME from "./time";
 
 // Define all admin commands
-const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, SUMMON, CLEAR, KICK, TIME];
+const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, SUMMON];
 
 export { ADMIN_COMMANDS };
