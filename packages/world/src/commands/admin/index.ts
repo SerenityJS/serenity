928e1c7f6528f0b850f18eb19edd4ea11af2916a
PMK744 <paytonmk744@outlook.com>
2024-06-17 19:25:42 -0500

    feat(world): added fill command

--- packages/world/src/commands/admin/index.ts
+++ packages/world/src/commands/admin/index.ts
@@ -1,11 +1,12 @@
 import GAMEMODE from "./gamemode";
 import SETBLOCK from "./setblock";
 import GIVE from "./give";
+import FILL from "./fill";
 import SUMMON from "./summon";
 import KICK from "./kick";
 import CLEAR from "./clear";
 
 // Define all admin commands
-const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, SUMMON, KICK, CLEAR];
+const ADMIN_COMMANDS = [GAMEMODE, SETBLOCK, GIVE, FILL, SUMMON, KICK, CLEAR];
 
 export { ADMIN_COMMANDS };
