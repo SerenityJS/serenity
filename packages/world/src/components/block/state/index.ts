d5e4cd3dbb0be56124514214ebef061099f830fc
PMK744 <paytonmk744@outlook.com>
2024-07-23 13:19:58 -0500

    feat(world): added BlockDirectionComponent

--- packages/world/src/components/block/state/index.ts
+++ packages/world/src/components/block/state/index.ts
@@ -1,3 +1,5 @@
+export * from "./state";
 export * from "./cardinal-direction";
 export * from "./weirdo-direction";
 export * from "./upside-down-bit";
+export * from "./direction";
---
+++ packages/world/src/components/block/state/state.ts
@@ -0,0 +1,7 @@
+import { BlockComponent } from "../block-component";
+
+class BlockStateComponent extends BlockComponent {
+	public static readonly state: string;
+}
+
+export { BlockStateComponent };
