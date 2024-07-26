0488b4b54861cdf49dba2c724ae8ec142cd9cfdd
nobu-sh <chat@nobu.sh>
2024-07-25 21:26:01 -0500

    feat(network): session encryption getters/setters

--- packages/network/src/session.ts
+++ packages/network/src/session.ts
@@ -59,35 +59,125 @@ class NetworkSession extends Emitter<NetworkEvents> {
 	 * Client public key for establishing encryption derived
 	 * from the last JWT token in the identity chain.
 	 */
-	public identityPublicKey: string | null = null;
+	private _identityPublicKey: string | null = null;
+	public get identityPublicKey(): string {
+		if (this._identityPublicKey === null) {
+			throw new Error("Identity public key is not set.");
+		}
+
+		return this._identityPublicKey;
+	}
+
+	public set identityPublicKey(value: string) {
+		if (this._identityPublicKey !== null) {
+			throw new Error("Identity public key is already set.");
+		}
+
+		this._identityPublicKey = value;
+	}
 
 	/**
 	 * Shared secret for encryption generated via ECDH with the
 	 * clients public key and the servers private key.
 	 */
-	public encryptionSharedSecret: Buffer | null = null;
+	private _encryptionSharedSecret: Buffer | null = null;
+	public get encryptionSharedSecret(): Buffer {
+		if (this._encryptionSharedSecret === null) {
+			throw new Error("Encryption shared secret is not set.");
+		}
+
+		return this._encryptionSharedSecret;
+	}
+
+	public set encryptionSharedSecret(value: Buffer) {
+		if (this._encryptionSharedSecret !== null) {
+			throw new Error("Encryption shared secret is already set.");
+		}
+
+		this._encryptionSharedSecret = value;
+	}
 
 	/**
 	 * Secret bytes for encryption generated via SHA256 with the
 	 * salt shared in the ServerToClientHandshake packet.
 	 */
-	public encryptionSecretBytes: Buffer | null = null;
+	private _encryptionSecretBytes: Buffer | null = null;
+	public get encryptionSecretBytes(): Buffer {
+		if (this._encryptionSecretBytes === null) {
+			throw new Error("Encryption secret bytes are not set.");
+		}
+
+		return this._encryptionSecretBytes;
+	}
+
+	public set encryptionSecretBytes(value: Buffer) {
+		if (this._encryptionSecretBytes !== null) {
+			throw new Error("Encryption secret bytes are already set.");
+		}
+
+		this._encryptionSecretBytes = value;
+	}
 
 	/**
 	 * Encryption initialization vector for encryption generated
 	 * via SHA256 with the salt shared in the ServerToClientHandshake packet.
 	 */
-	public encryptionInitVector: Buffer | null = null;
+	private _encryptionInitVector: Buffer | null = null;
+	public get encryptionInitVector(): Buffer {
+		if (this._encryptionInitVector === null) {
+			throw new Error("Encryption initialization vector is not set.");
+		}
+
+		return this._encryptionInitVector;
+	}
+
+	public set encryptionInitVector(value: Buffer) {
+		if (this._encryptionInitVector !== null) {
+			throw new Error("Encryption initialization vector is already set.");
+		}
+
+		this._encryptionInitVector = value;
+	}
 
 	/**
 	 * The cipher instance for encrypting bytes.
 	 */
-	public cipher: Cipher | null = null;
+	private _cipher: Cipher | null = null;
+	public get cipher(): Cipher {
+		if (this._cipher === null) {
+			throw new Error("Cipher is not set.");
+		}
+
+		return this._cipher;
+	}
+
+	public set cipher(value: Cipher) {
+		if (this._cipher !== null) {
+			throw new Error("Cipher is already set.");
+		}
+
+		this._cipher = value;
+	}
 
 	/**
 	 * The decipher instance for decrypting bytes.
 	 */
-	public decipher: Cipher | null = null;
+	private _decipher: Cipher | null = null;
+	public get decipher(): Cipher {
+		if (this._decipher === null) {
+			throw new Error("Decipher is not set.");
+		}
+
+		return this._decipher;
+	}
+
+	public set decipher(value: Cipher) {
+		if (this._decipher !== null) {
+			throw new Error("Decipher is already set.");
+		}
+
+		this._decipher = value;
+	}
 
 	/**
 	 * Encrypted packet send sequence number.
