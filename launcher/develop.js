d6ac22b06d109381bb5ed0a606851d7e8cf86a5e
Felipe-Devr <felipegamerdev1@gmail.com>
2024-07-26 17:18:49 -0700

    Reapply "chore: removed launcher"
    This reverts commit b7183a47c8669b28239903ad360bb9b3224755a4.

--- launcher/develop.js
+++
@@ -1,3 +0,0 @@
-const { Serenity } = require(".");
-const serenity = new Serenity();
-serenity.start();
--- launcher/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "@serenityjs/serenity";
-
-console.log(
-	"WARNING: Package '@serenityjs/launcher' is deprecated. Please use '@serenityjs/serenity' instead. If you are using the SerenityJS executable, please download the latest version from https://github.com/SerenityJS/executable/"
-);
