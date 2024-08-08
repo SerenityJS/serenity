188e9a475d630a2d4a54c90ae76b42b7dd50a388
Felipe-Devr <felipegamerdev1@gmail.com>
2024-08-08 15:49:43 -0700

    feat(world): added /kill command

--- packages/world/src/commands/admin/index.ts
+++ packages/world/src/commands/admin/index.ts
@@ -7,6 +7,7 @@ import KICK from "./kick";
 import CLEAR from "./clear";
 import ABILITY from "./ability";
 import TP from "./tp";
+import KILL from "./kill";
 
 // Define all admin commands
 const ADMIN_COMMANDS = [
@@ -18,7 +19,8 @@ const ADMIN_COMMANDS = [
 	KICK,
 	CLEAR,
 	ABILITY,
-	TP
+	TP,
+	KILL
 ];
 
 export { ADMIN_COMMANDS };
---
+++ packages/world/src/commands/admin/kill.ts
@@ -0,0 +1,54 @@
+import { CommandPermissionLevel, Gamemode } from "@serenityjs/protocol";
+
+import { TargetEnum } from "../enums";
+import { Dimension, type World } from "../../world";
+
+import type { Entity } from "../../entity";
+import type { Player } from "../../player";
+
+const entityFilter = (entity: Entity) =>
+	entity.isAlive &&
+	(entity.isPlayer() ? entity.gamemode != Gamemode.Creative : true);
+
+const register = (world: World) => {
+	// Register the kill command
+	world.commands.register(
+		"kill",
+		"Kills entities (players, mobs, etc.).",
+		(origin, parameters) => {
+			// ? Get the valid entities
+			const entities = parameters.target.result.filter((entity) =>
+				entityFilter(entity)
+			);
+
+			if (parameters.target.result.some((entity) => entityFilter(entity))) {
+				if (origin instanceof Dimension) {
+					console.info(
+						"Players cannot be killed while they are in Creative mode."
+					);
+				} else
+					(origin as Player).sendMessage(
+						"Players cannot be killed while they are in Creative mode."
+					);
+			}
+
+			for (const target of entities) {
+				// ? Kill the target is is alive or if the player is not in creative mode
+				target.kill();
+			}
+
+			// Send the success message
+			return {
+				message: `Killed ${entities.map((entity) => (entity.isPlayer() ? entity.username : entity.type.identifier)).join(", ")}`
+			};
+		},
+		{
+			target: TargetEnum
+		},
+		{
+			permission: CommandPermissionLevel.Operator
+		}
+	);
+};
+
+export default register;
