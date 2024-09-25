bafa1e334a668881351fbb243a3c0b99c9cb441c
Palm <christopher.k.izatt@gmail.com>
2024-09-24 22:32:32 -0400

    Furthered vanilla parity with commands.
    - Added MCIdentifier class for formatting and parsing IDs from and to displayable strings.
    - Modified functionality and messages in all existing commands to better parity vanilla commands.

--- packages/world/src/commands/admin/clear.ts
+++ packages/world/src/commands/admin/clear.ts
@@ -5,82 +5,94 @@ import { ItemEnum, TargetEnum } from "../enums";
 
 import type { Entity } from "../../entity";
 import type { World } from "../../world";
+import { EntityIdentifier, EntityType } from "@serenityjs/entity";
+import { MCIdentifier } from "../../utils/identifier";
 
 const register = (world: World) => {
-	// Register the clear command
-	world.commands.register(
-		"clear",
-		"Clears items from player inventory.",
-		(registry) => {
-			// Set the command to be an operator command
-			registry.permissionLevel = CommandPermissionLevel.Operator;
+  // Register the clear command
+  world.commands.register(
+    "clear",
+    "Clears items from player inventory.",
+    (registry) => {
+      // Set the command to be an operator command
+      registry.permissionLevel = CommandPermissionLevel.Operator;
 
-			// Create an overload for the command
-			registry.overload(
-				{
-					target: TargetEnum,
-					item: [ItemEnum, true],
-					metadata: [IntegerEnum, true],
-					amount: [IntegerEnum, true]
-				},
-				(context) => {
-					// Get the targets from the context
-					const targets = context.target.result as Array<Entity>;
+      // Create an overload for the command
+      registry.overload(
+        {
+          player: TargetEnum,
+          itemName: [ItemEnum, true],
+          data: [IntegerEnum, true],
+          amount: [IntegerEnum, true],
+        },
+        (context) => {
+          // Get the targets from the context
+          const targets = context.player.result as Array<Entity>;
 
-					// Get the result of the item, amount, and metadata
-					const itemResult = context.item?.result as string | undefined;
-					const itemIdentifier = itemResult
-						? `${itemResult.includes(":") ? "" : "minecraft:"}${itemResult}`
-						: undefined;
-					const itemMetadata = context.metadata?.result ?? 0;
-					const itemAmount = context.amount?.result;
-					let itemCount = 0;
+          // Get the result of the item, amount, and metadata
+          const itemResult = context.itemName?.result as string | undefined;
+          const itemIdentifier = itemResult
+            ? `${itemResult.includes(":") ? "" : "minecraft:"}${itemResult}`
+            : undefined;
+          const itemMetadata = context.data?.result ?? 0;
+          const itemAmount = context.amount?.result;
+          let itemCount = 0;
 
-					if (targets.length === 0) {
-						return { message: "No targets matched selector" };
-					}
+          // Create an array to hold the log messages
+          const logs: Array<string> = [];
 
-					for (const target of targets) {
-						if (!target.hasComponent("minecraft:inventory")) continue;
-						const { container } = target.getComponent("minecraft:inventory");
+          if (targets.length === 0) {
+            return { message: "No targets matched selector" };
+          }
 
-						if (!itemResult) {
-							container.clear();
-							continue;
-						}
+          for (const target of targets) {
+            if (!target.hasComponent("minecraft:inventory")) continue;
+            const { container } = target.getComponent("minecraft:inventory");
 
-						for (const [slot, itemStack] of Object.entries(container.storage)) {
-							if (
-								!itemStack ||
-								(itemStack.type.identifier !== itemIdentifier &&
-									itemStack.metadata !== itemMetadata)
-							) {
-								continue;
-							}
+            if (!itemResult) {
+              container.clear();
+              continue;
+            }
 
-							const stackAmount = itemStack.amount;
-							const remaining = stackAmount - (itemAmount ?? 1);
+            for (const [slot, itemStack] of Object.entries(container.storage)) {
+              if (
+                !itemStack ||
+                (itemStack.type.identifier !== itemIdentifier &&
+                  itemStack.metadata !== itemMetadata)
+              ) {
+                continue;
+              }
 
-							if (remaining < 0) {
-								container.clearSlot(Number.parseInt(slot));
-								itemCount += stackAmount;
-							} else {
-								itemStack.setAmount(Math.max(remaining, 0));
-								itemCount += Math.min(itemAmount ?? 1, stackAmount);
-							}
+              const stackAmount = itemStack.amount;
+              const remaining = stackAmount - (itemAmount ?? 1);
 
-							if (itemCount >= (itemAmount ?? 1)) break;
-						}
-					}
+              if (remaining < 0) {
+                container.clearSlot(Number.parseInt(slot));
+                itemCount += stackAmount;
+              } else {
+                itemStack.setAmount(Math.max(remaining, 0));
+                itemCount += Math.min(itemAmount ?? 1, stackAmount);
+              }
 
-					return {
-						message: `Cleared the inventory of ${targets.length} entities`
-					};
-				}
-			);
-		},
-		() => {}
-	);
+              if (itemCount >= (itemAmount ?? 1)) break;
+            }
+            if (itemCount === 0)
+              logs.push(
+                `§cCould not clear the inventory of ${target.getNametag() ?? MCIdentifier.format(target.type.identifier)}, no items to remove§r`
+              );
+            else
+              logs.push(
+                `§fCleared the inventory of ${target.getNametag() ?? MCIdentifier.format(target.type.identifier)}, removing ${itemCount} items§r`
+              );
+          }
+          return {
+            message: logs.join("\n"),
+          };
+        }
+      );
+    },
+    () => {}
+  );
 };
 
 export default register;
--- packages/world/src/commands/admin/gamemode.ts
+++ packages/world/src/commands/admin/gamemode.ts
@@ -4,86 +4,95 @@ import { GamemodeEnum, TargetEnum } from "../enums";
 import { Player } from "../../player";
 
 import type { World } from "../../world";
+import { Entity } from "../../entity";
 
 const register = (world: World) => {
-	// Register the about command
-	world.commands.register(
-		"gamemode",
-		"Sets a player's gamemode",
-		(registry) => {
-			// Set the command to be an operator command
-			registry.permissionLevel = CommandPermissionLevel.Operator;
+  // Register the about command
+  world.commands.register(
+    "gamemode",
+    "Sets a player's gamemode.",
+    (registry) => {
+      // Set the command to be an operator command
+      registry.permissionLevel = CommandPermissionLevel.Operator;
 
-			// Create an overload for the command
-			registry.overload(
-				{
-					gamemode: GamemodeEnum,
-					target: [TargetEnum, true]
-				},
-				(context) => {
-					// Get the targets from the context
-					const targets = context.target.validate()
-						? context.target.result
-						: [context.origin instanceof Player ? context.origin : null];
+      // Create an overload for the command
+      registry.overload(
+        {
+          gameMode: GamemodeEnum,
+          player: [TargetEnum, true],
+        },
+        (context) => {
+          // Get the targets from the context
+          const targets = context.player.validate()
+            ? context.player.result
+            : [context.origin instanceof Player ? context.origin : null];
 
-					// Check if there are no targets
-					if (!targets || targets.length === 0)
-						throw new Error("No targets matched the selector.");
+          // Check if there are no targets
+          if (!targets || targets.length === 0)
+            throw new Error("No targets matched the selector.");
 
-					// Get the gamemode from the context
-					const gamemode = context.gamemode.result as string;
+          // Get the gamemode from the context
+          const gamemode = context.gameMode.result as string;
+          let mode = gamemode.toLowerCase();
 
-					// Create an array to hold the log messages
-					const logs: Array<string> = [];
+          // Create an array to hold the log messages
+          const logs: Array<string> = [];
 
-					// Loop through all the targets
-					for (const target of targets) {
-						// Check if the target is not a player
-						if (!target || !(target instanceof Player)) continue;
+          // Loop through all the targets
+          for (const target of targets) {
+            // Check if the target is not a player
+            if (!target || !(target instanceof Player)) continue;
 
-						switch (gamemode) {
-							case "s":
-							case "survival": {
-								target.setGamemode(Gamemode.Survival);
-								break;
-							}
+            switch (gamemode) {
+              case "s":
+              case "survival": {
+                target.setGamemode(Gamemode.Survival);
+                mode = "survival";
+                break;
+              }
 
-							case "c":
-							case "creative": {
-								target.setGamemode(Gamemode.Creative);
-								break;
-							}
+              case "c":
+              case "creative": {
+                target.setGamemode(Gamemode.Creative);
+                mode = "creative";
+                break;
+              }
 
-							case "a":
-							case "adventure": {
-								target.setGamemode(Gamemode.Adventure);
-								break;
-							}
+              case "a":
+              case "adventure": {
+                target.setGamemode(Gamemode.Adventure);
+                mode = "adventure";
+                break;
+              }
 
-							case "sp":
-							case "spectator": {
-								target.setGamemode(Gamemode.Spectator);
-								break;
-							}
+              case "sp":
+              case "spectator": {
+                target.setGamemode(Gamemode.Spectator);
+                mode = "spectator";
+                break;
+              }
 
-							default: {
-								throw new TypeError("Invalid gamemode specified!");
-							}
-						}
+              default: {
+                throw new TypeError("Gamemode '" + gamemode + "' is invalid");
+              }
+            }
 
-						// Add the log message
-						logs.push(
-							`§7Successfully updated §a${target.username}'s§7 gamemode!§r`
-						);
-					}
+            // Add the log message
+            if (
+              context.origin instanceof Player &&
+              target.username == context.origin.username
+            )
+              logs.push(`§fSet own gamemode to ${mode}`);
+            else logs.push(`§fSet ${target.username}'s gamemode to ${mode}`);
+          }
 
-					// Return the success message
-					return { message: logs.join("\n") };
-				}
-			);
-		},
-		() => {}
-	);
+          // Return the success message
+          return { message: logs.join("\n") };
+        }
+      );
+    },
+    () => {}
+  );
 };
 
 export default register;
