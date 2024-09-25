1aa383b425c1510d46e4b1f14677ef9920bfbef6
Palm <christopher.k.izatt@gmail.com>
2024-09-24 23:02:08 -0400

    Revert "Furthered vanilla parity for some commands."
    This reverts commit 61c16b457f5edb1d6b590d7970431c444a405317.

--- packages/world/src/commands/admin/setblock.ts
+++ packages/world/src/commands/admin/setblock.ts
@@ -7,52 +7,49 @@ import { Entity } from "../../entity";
 import type { World } from "../../world";
 
 const register = (world: World) => {
-  // Register the setblock command
-  world.commands.register(
-    "setblock",
-    "Changes a block to another block.",
-    (registry) => {
-      // Set the command to be an operator command
-      registry.permissionLevel = CommandPermissionLevel.Operator;
+	// Register the setblock command
+	world.commands.register(
+		"setblock",
+		"Sets a block at the specified location",
+		(registry) => {
+			// Set the command to be an operator command
+			registry.permissionLevel = CommandPermissionLevel.Operator;
 
-      // Create an overload for the command
-      registry.overload(
-        {
-          position: PositionEnum,
-          block: BlockEnum,
-        },
-        (context) => {
-          // Get the result of the block, position, and mode
-          const result = context.block.result as string;
-          const identifier = result.includes(":")
-            ? result
-            : `minecraft:${result}`;
+			// Create an overload for the command
+			registry.overload(
+				{
+					position: PositionEnum,
+					block: BlockEnum
+				},
+				(context) => {
+					// Get the result of the block, position, and mode
+					const result = context.block.result as string;
+					const identifier = result.includes(":")
+						? result
+						: `minecraft:${result}`;
 
-          // Get the position from the context
-          const position = context.position.result?.floor() as Vector3f;
+					// Get the position from the context
+					const position = context.position.result?.floor() as Vector3f;
 
-          // Get the dimension based on the origin
-          const dimension =
-            context.origin instanceof Entity
-              ? context.origin.dimension
-              : context.origin;
+					// Get the dimension based on the origin
+					const dimension =
+						context.origin instanceof Entity
+							? context.origin.dimension
+							: context.origin;
 
-          // Get the block at the specified location
-          const block = dimension.getBlock(position);
+					// Get the block at the specified location
+					const block = dimension.getBlock(position);
 
-          // Get the block type from the identifier
-          const type = BlockType.get(identifier as BlockIdentifier);
+					// Get the block type from the identifier
+					const type = BlockType.get(identifier as BlockIdentifier);
 
-          // Set the block at the specified location
-          block.setType(type);
-          return {
-            message: "§fBlock placed",
-          };
-        }
-      );
-    },
-    () => {}
-  );
+					// Set the block at the specified location
+					block.setType(type);
+				}
+			);
+		},
+		() => {}
+	);
 };
 
 export default register;
--- packages/world/src/commands/admin/summon.ts
+++ packages/world/src/commands/admin/summon.ts
@@ -5,92 +5,75 @@ import { Entity } from "../../entity";
 
 import type { EntityIdentifier } from "@serenityjs/entity";
 import type { World } from "../../world";
-import { StringEnum } from "@serenityjs/command";
 
 const register = (world: World) => {
-  // Register the setblock command
-  world.commands.register(
-    "summon",
-    "Summons an entity.",
-    (registry) => {
-      // Set the command to be an operator command
-      registry.permissionLevel = CommandPermissionLevel.Operator;
+	// Register the setblock command
+	world.commands.register(
+		"summon",
+		"Summons an entity at a specified location",
+		(registry) => {
+			// Set the command to be an operator command
+			registry.permissionLevel = CommandPermissionLevel.Operator;
 
-      // Create an overload for the command
-      registry.overload(
-        {
-          entity: EntityEnum,
-          nameTag: [StringEnum, true],
-        },
-        (context) => {
-          // Check if the origin is a dimension, if so, throw an error
-          if (!(context.origin instanceof Entity))
-            throw new Error(
-              "You can't use this command in this context, try adding a position argument."
-            );
+			// Create an overload for the command
+			registry.overload(
+				{
+					entity: EntityEnum
+				},
+				(context) => {
+					// Check if the origin is a dimension, if so, throw an error
+					if (!(context.origin instanceof Entity))
+						throw new Error(
+							"You can't use this command in this context, try adding a position argument."
+						);
 
-          // Get the result of the entity
-          const identifier = (context.entity.result as string).includes(":")
-            ? (context.entity.result as EntityIdentifier)
-            : (`minecraft:${context.entity.result}` as EntityIdentifier);
+					// Get the result of the entity
+					const identifier = (context.entity.result as string).includes(":")
+						? (context.entity.result as EntityIdentifier)
+						: (`minecraft:${context.entity.result}` as EntityIdentifier);
 
-          // Get the position of the entity
-          const { x, y, z } = context.origin.position.floor();
+					// Get the position of the entity
+					const { x, y, z } = context.origin.position.floor();
 
-          // Spawn the entity at the specified location
-          const living = context.origin.dimension.spawnEntity(
-            identifier,
-            new Vector3f(x, y, z)
-          );
+					// Spawn the entity at the specified location
+					context.origin.dimension.spawnEntity(
+						identifier,
+						new Vector3f(x, y, z)
+					);
 
-          // Set nametag (if provided)
-          if (context.nameTag.result) living.setNametag(context.nameTag.result);
+					// Send the success message
+					return {
+						message: `Successfully summoned entity at ${x}, ${y}, ${z}!`
+					};
+				}
+			);
 
-          // Send the success message
-          return {
-            message: `§fEntity successfully summoned`,
-          };
-        }
-      );
+			// Create an overload for the command
+			registry.overload(
+				{
+					entity: EntityEnum,
+					position: PositionEnum
+				},
+				(context) => {
+					// Get the result of the entity and position
+					const identifier = (context.entity.result as string).includes(":")
+						? (context.entity.result as EntityIdentifier)
+						: (`minecraft:${context.entity.result}` as EntityIdentifier);
+					const { x, y, z } = context.position.result as Vector3f;
 
-      // Create an overload for the command
-      registry.overload(
-        {
-          entity: EntityEnum,
-          position: PositionEnum,
-          nameTag: [StringEnum, true],
-        },
-        (context) => {
-          // Get the result of the entity and position
-          const identifier = (context.entity.result as string).includes(":")
-            ? (context.entity.result as EntityIdentifier)
-            : (`minecraft:${context.entity.result}` as EntityIdentifier);
-          const { x, y, z } = context.position.result as Vector3f;
+					// Get the dimension based on the origin
+					const dimension =
+						context.origin instanceof Entity
+							? context.origin.dimension
+							: context.origin;
 
-          // Get the dimension based on the origin
-          const dimension =
-            context.origin instanceof Entity
-              ? context.origin.dimension
-              : context.origin;
-
-          // Summon the entity at the specified location
-          const living = dimension.spawnEntity(
-            identifier,
-            new Vector3f(x, y, z)
-          );
-
-          // Set nametag (if provided)
-          if (context.nameTag.result) living.setNametag(context.nameTag.result);
-
-          // Send the success message
-          return {
-            message: `§fEntity successfully summoned at §7${x.toFixed(2)}§f, §7${y.toFixed(2)}§f, §7${z.toFixed(2)}§r`,
-          };
-        }
-      );
-    },
-    () => {}
-  );
+					// Summon the entity at the specified location
+					dimension.spawnEntity(identifier, new Vector3f(x, y, z));
+				}
+			);
+		},
+		() => {}
+	);
 };
 
 export default register;
