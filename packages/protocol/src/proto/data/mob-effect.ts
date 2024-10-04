df7408ffd446c2920499585b24c616df7e36568f
Felipe-Devr <felipegamerdev1@gmail.com>
2024-10-04 14:18:31 -0700

    chore(proto): formatted mob-effect.ts

--- packages/protocol/src/proto/data/mob-effect.ts
+++ packages/protocol/src/proto/data/mob-effect.ts
@@ -1,17 +1,26 @@
-import { Bool, Byte, Uint64, VarInt, VarLong, ZigZag } from "@serenityjs/binarystream";
+import {
+	Bool,
+	Byte,
+	Uint64,
+	VarInt,
+	VarLong,
+	ZigZag
+} from "@serenityjs/binarystream";
 import { Proto, Serialize } from "@serenityjs/raknet";
-import { DataPacket } from "./data-packet";
+
 import { EffectType, MobEffectEvents, Packet } from "../../enums";
+
+import { DataPacket } from "./data-packet";
 
 @Proto(Packet.MobEffect)
 class MobEffectPacket extends DataPacket {
-  @Serialize(VarLong) public runtimeId!: bigint;
-  @Serialize(Byte) public eventId!: MobEffectEvents;
-  @Serialize(ZigZag) public effectId!: EffectType;
-  @Serialize(VarInt) public amplifier!: number;
-  @Serialize(Bool) public particles!: boolean;
-  @Serialize(VarInt) public duration!: number;
-  @Serialize(Uint64) public tick!: bigint;
+	@Serialize(VarLong) public runtimeId!: bigint;
+	@Serialize(Byte) public eventId!: MobEffectEvents;
+	@Serialize(ZigZag) public effectId!: EffectType;
+	@Serialize(VarInt) public amplifier!: number;
+	@Serialize(Bool) public particles!: boolean;
+	@Serialize(VarInt) public duration!: number;
+	@Serialize(Uint64) public tick!: bigint;
 }
 
 export { MobEffectPacket };
