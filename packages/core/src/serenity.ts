0324de6f7a1ca31b86c7b13445599fde520556dc
PMK744 <paytonmk744@outlook.com>
2024-11-17 13:44:58 -0600

    chore: fix typo

--- packages/core/src/serenity.ts
+++ packages/core/src/serenity.ts
@@ -25,7 +25,6 @@ import { ServerEvent } from "./enums";
 import { ResourcePackManager } from "./resource-packs";
 
 import type {
-  ResourcePacksProperties,
   ServerEvents,
   ServerProperties,
   WorldEventSignals,
