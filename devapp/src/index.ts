ee3b9483b030476d930134842da0d1da9c919d71
PMK744 <paytonmk744@outlook.com>
2024-12-17 21:20:23 -0600

    chore: remove testing code

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,10 +1,4 @@
-import {
-  Serenity,
-  LevelDBProvider,
-  BlockTrait,
-  BlockIdentifier,
-  WorldEvent
-} from "@serenityjs/core";
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
 // Create a new Serenity instance
@@ -13,19 +7,6 @@ const serenity = new Serenity({
   permissions: "./permissions.json",
   resourcePacks: "./resource_packs",
   debugLogging: true
-});
-
-class CustomBlockTrait extends BlockTrait {
-  public static readonly identifier = "custom";
-  public static readonly types = [BlockIdentifier.Cobblestone];
-
-  public onTick(deltaTick: number): void {
-    this.block.world.sendMessage(`tick: ${deltaTick}`);
-  }
-}
-
-serenity.on(WorldEvent.WorldInitialize, ({ world }) => {
-  world.blockPalette.registerTrait(CustomBlockTrait);
 });
 
 // Create a new plugin pipeline
