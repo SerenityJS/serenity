02b2e8cf1d9f00a45c4122f78563efb1b884cf7a
João <smallkingdev@gmail.com>
2024-12-19 17:24:46 +0000

    feat(core): implement  ItemKeepOnDieTrait

---
+++ packages/core/src/item/traits/keep-on-die.ts
@@ -0,0 +1,25 @@
+import { ByteTag } from "@serenityjs/nbt";
+
+import { ItemIdentifier } from "../../enums";
+
+import { ItemTrait } from "./trait";
+
+class ItemKeepOnDieTrait<T extends ItemIdentifier> extends ItemTrait<T> {
+  public static readonly identifier = "keep_on_die";
+
+  public get keep(): boolean {
+    return this.item.nbt.has("minecraft:keep_on_death");
+  }
+
+  public set keep(value: boolean) {
+    if (value) {
+      this.item.nbt.add(
+        new ByteTag({ name: "minecraft:keep_on_death", value: 1 })
+      );
+    } else {
+      this.item.nbt.delete("minecraft:keep_on_death");
+    }
+  }
+}
+
+export { ItemKeepOnDieTrait };
