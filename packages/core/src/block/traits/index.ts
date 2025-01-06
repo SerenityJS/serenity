61754a08a6e0d734f10075647b573d3b1162ba28
SkyderG <skydergamer21@gmail.com>
2025-01-06 13:32:09 -0300

    refactor(protocol): change container id type to int8

--- packages/core/src/block/traits/index.ts
+++ packages/core/src/block/traits/index.ts
@@ -7,5 +7,4 @@ export * from "./upper-block-bit";
 export * from "./vertical-half";
 export * from "./cardinal_direction";
 export * from "./direction";
-export * from "./furnace";
 export * from "./facing_direction";
