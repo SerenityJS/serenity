7c63d194910c38d8a3e3e8e80f8e3a6179a7bc53
bonanoo <ainaraspirmas@gmail.com>
2025-02-21 11:03:07 +0200

    SubChunk Blocks DataType

---
+++ packages/protocol/src/proto/types/subchunk-blocks.ts
@@ -0,0 +1,34 @@
+import { BinaryStream, Endianness } from "@serenityjs/binarystream";
+import { DataType } from "@serenityjs/raknet";
+import { BlockUpdate } from "./block-update";
+
+export class SubchunkBlocks extends DataType {
+	public blocks: BlockUpdate[] = [];
+
+  public constructor(blocks: Array<BlockUpdate>) {
+    super();
+    this.blocks = blocks;
+  }
+
+	public static override write(
+    stream: BinaryStream,
+    value: SubchunkBlocks,
+    _: Endianness
+  ): void {
+    stream.writeVarInt(value.blocks.length);
+    for (const block of value.blocks) {
+      BlockUpdate.write(stream, block, _);
+    }
+  }
+
+	public static read(stream: BinaryStream): SubchunkBlocks {
+		const blocks: BlockUpdate[] = [];
+		const length = stream.readVarInt();
+		for (let i = 0; i < length; i++) {
+			const block = BlockUpdate.read(stream);
+			blocks.push(block);
+		}
+		const subchunk = new SubchunkBlocks(blocks);
+		return subchunk;
+	}
+}
