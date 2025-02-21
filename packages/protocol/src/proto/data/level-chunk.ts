8d318f76eb0bd01db594993830e84be2c907e03d
bonanoo <ainaraspirmas@gmail.com>
2025-02-21 11:05:00 +0200

    Update LevelChunk Packet

--- packages/protocol/src/proto/data/level-chunk.ts
+++ packages/protocol/src/proto/data/level-chunk.ts
@@ -10,62 +10,68 @@ import type { Buffer } from "node:buffer";
 
 @Proto(Packet.LevelChunk)
 class LevelChunkPacket extends DataPacket {
+  private static readonly MAX_BLOB_HASHES = 64;
+
   public x!: number;
   public z!: number;
   public dimension!: DimensionType;
+  public highestSubChunkCount!: number;
   public subChunkCount!: number;
   public cacheEnabled!: boolean;
   public blobs!: Array<bigint>;
   public data!: Buffer;
 
-  public override serialize(): Buffer {
+	public override serialize(): Buffer {
     this.writeVarInt(Packet.LevelChunk);
     this.writeZigZag(this.x);
     this.writeZigZag(this.z);
     this.writeZigZag(this.dimension);
-    if (this.cacheEnabled) {
-      this.writeVarInt(-2);
-      this.writeUint16(this.subChunkCount, Endianness.Little);
-    } else {
-      this.writeVarInt(this.subChunkCount);
+    this.writeVarInt(this.subChunkCount);
+    if (this.subChunkCount === -2) {
+      this.writeUint16(this.highestSubChunkCount ?? 0, Endianness.Little);
     }
-
     this.writeBool(this.cacheEnabled);
-    if (this.blobs) {
+    if (this.cacheEnabled) {
+      if (!this.blobs)
+        throw new Error("Blobs required when cache_enabled is true");
       this.writeVarInt(this.blobs.length);
-      for (const hash of this.blobs) {
-        this.writeUint64(hash, Endianness.Little);
-      }
+			for (const hash of this.blobs) {
+				this.writeUint64(hash, Endianness.Little);
+			}
     }
-
-    this.writeVarInt(this.data.byteLength);
+    this.writeVarInt(this.data.length);
     this.writeBuffer(this.data);
-
     return this.getBuffer();
   }
 
-  public override deserialize(): this {
-    this.readVarInt();
-    this.x = this.readZigZag();
-    this.z = this.readZigZag();
-    this.dimension = this.readZigZag();
-    this.subChunkCount = this.readVarInt();
+
+	public override deserialize(): this {
+		this.readVarInt(); // packet id
+		this.x = this.readZigZag();
+		this.z = this.readZigZag();
+		this.dimension = this.readZigZag();
+		this.subChunkCount = this.readVarInt();
+		if (this.subChunkCount === 4294967294) this.subChunkCount = -2;
+
     if (this.subChunkCount === -2) {
-      this.subChunkCount = this.readUint16(Endianness.Little);
+      this.highestSubChunkCount = this.readUint16(Endianness.Little);
     }
 
     this.cacheEnabled = this.readBool();
     if (this.cacheEnabled) {
       const blobCount = this.readVarInt();
-      this.blobs = [];
-      for (let index = 0; index < blobCount; index++) {
-        this.blobs.push(this.readUint64(Endianness.Little));
-      }
-    }
+      if (blobCount > LevelChunkPacket.MAX_BLOB_HASHES) {
+        throw new Error(`Too many blob hashes: ${blobCount}`);
+			}
 
-    const length = this.readVarInt();
-    this.data = this.readBuffer(length);
+			this.blobs = [];
+			for (let index = 0; index < blobCount; index++) {
+				this.blobs.push(this.readLong(Endianness.Little));
+			}
+		}
 
+    const dataLength = this.readVarInt();
+    this.data = this.readBuffer(dataLength);
     return this;
   }
 }
