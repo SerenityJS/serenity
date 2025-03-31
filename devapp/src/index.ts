6524bd7fd0f57682e420c4e60d9a949461038b4c
Payton Kerby <paytonmk744@outlook.com>
2025-03-31 18:57:38 -0500

    chore: remove test code

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,10 +1,4 @@
-import {
-  Serenity,
-  LevelDBProvider,
-  WorldEvent,
-  ItemType,
-  ItemStack
-} from "@serenityjs/core";
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
 // Create a new Serenity instance
@@ -15,25 +9,6 @@ const serenity = new Serenity({
     resourcePacks: "./resource_packs",
     debugLogging: true
   }
-});
-
-serenity.on(WorldEvent.WorldInitialize, ({ world }) => {
-  world.commandPalette.register("test", "", () => {
-    const dimension = world.getDimension("overworld");
-
-    const amount = ItemType.types.size;
-    const itemTypes = Array.from(ItemType.types.values());
-
-    for (let x = -20; x <= 20; x++) {
-      for (let z = -20; z <= 20; z++) {
-        const index = Math.floor(Math.random() * amount);
-
-        const type = itemTypes[index]!;
-
-        dimension?.spawnItem(new ItemStack(type), { x, y: 100, z });
-      }
-    }
-  });
 });
 
 // Create a new plugin pipeline
