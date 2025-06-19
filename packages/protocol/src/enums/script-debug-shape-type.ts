fe7b11bdaa6a9d92d0e765f7a01d0ef29f253842
Payton Kerby <paytonmk744@outlook.com>
2025-06-19 13:15:59 -0500

    chore(protocol): fixed `ScriptDebugShapeType` enum

--- packages/protocol/src/enums/script-debug-shape-type.ts
+++ packages/protocol/src/enums/script-debug-shape-type.ts
@@ -1,10 +1,10 @@
 enum ScriptDebugShapeType {
-  Line = 1,
-  Box = 2,
-  Sphere = 3,
-  Circle = 4,
-  Text = 5,
-  Arrow = 6,
+  Line = 0,
+  Box = 1,
+  Sphere = 2,
+  Circle = 3,
+  Text = 4,
+  Arrow = 5,
   NumShapeTypes
 }
 
