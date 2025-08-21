d0fd9ac9ce763a5e470a5f5a95228f38ac16d593
Payton Kerby <paytonmk744@outlook.com>
2025-08-21 17:54:44 -0500

    chore: remove dummy code

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,10 +1,4 @@
-import {
-  Serenity,
-  LevelDBProvider,
-  WorldEvent,
-  Player,
-  PlayerLevelingTrait
-} from "@serenityjs/core";
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
 // Create a new Serenity instance
@@ -14,26 +8,6 @@ const serenity = new Serenity({
     permissions: "./permissions.json",
     debugLogging: true
   }
-});
-
-serenity.on(WorldEvent.PlayerJoin, ({ player }) => {
-  player.setExperience(0);
-  player.setLevel(0);
-});
-
-serenity.on(WorldEvent.WorldInitialize, ({ world }) => {
-  let total = 0;
-
-  world.commandPalette.register("test", "", ({ origin }) => {
-    if (!(origin instanceof Player)) return;
-    // Generate a random level and experience from 0.0 to 10.0
-    const randomLevel = Math.random() * 10;
-
-    origin.addExperience(randomLevel);
-    total += randomLevel;
-
-    console.log(total);
-  });
 });
 
 // Create a new plugin pipeline
