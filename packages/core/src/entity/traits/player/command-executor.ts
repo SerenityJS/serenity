8b38c8e46e676624df3dac7699dc94ea3e79a8ed
Palm <christopher.k.izatt@gmail.com>
2025-09-17 16:57:25 -0400

    Re-work command system with enum support.

--- packages/core/src/entity/traits/player/command-executor.ts
+++ packages/core/src/entity/traits/player/command-executor.ts
@@ -1,16 +1,13 @@
 import {
   AvailableCommandsPacket,
-  CommandPermissionLevel
+  CommandPermissionLevel,
 } from "@serenityjs/protocol";
-
 import { EntityIdentifier } from "../../../enums";
-import { Command, type CustomEnum, SoftEnum } from "../../../commands";
-
+import { Command, CustomEnum, SoftEnum } from "../../../commands";
 import { PlayerTrait } from "./trait";
 
 class PlayerCommandExecutorTrait extends PlayerTrait {
   public static readonly identifier = "command-executor";
-
   public static readonly types = [EntityIdentifier.Player];
 
   /**
@@ -22,35 +19,33 @@ class PlayerCommandExecutorTrait extends PlayerTrait {
     const serenity = world.serenity;
 
     // Filter out all commands that the player does not have permission to execute
-    const availableCommands = world.commandPalette
-      .getAll()
-      .filter((command) => {
-        // Iterate over all groups and check if the player has the permission
-        for (const group of serenity.permissions.groups) {
-          // Iterate over all permissions in the group
-          for (const [permission, commands] of group.permissions) {
-            // Get the identifier of the permission
-            const identifier = `${group.identifier}.${permission}`;
+    const availableCommands = world.commandPalette.getAll().filter((command) => {
+      // Iterate over all groups and check if the player has the permission
+      for (const group of serenity.permissions.groups) {
+        // Iterate over all permissions in the group
+        for (const [permission, commands] of group.permissions) {
+          // Get the identifier of the permission
+          const identifier = `${group.identifier}.${permission}`;
 
-            // Check if the player has the permission and if the command is in the list of commands
-            if (
-              this.player.permissions.has(identifier) &&
-              commands.includes(command.name)
-            ) {
-              // Return true, as the player has a permission that allows them to execute the command
-              return true;
-            }
+          // Check if the player has the permission and if the command is in the list of commands
+          if (
+            this.player.permissions.has(identifier) &&
+            commands.includes(command.name)
+          ) {
+            // Return true, as the player has a permission that allows them to execute the command
+            return true;
           }
         }
+      }
 
-        // Iterate over all permissions required by the command
-        // And check if the player has the permission
-        for (const permission of command.registry.permissions)
-          if (!this.player.permissions.has(permission)) return false;
+      // Iterate over all permissions required by the command
+      // And check if the player has the permission
+      for (const permission of command.registry.permissions)
+        if (!this.player.permissions.has(permission)) return false;
 
-        // Return true, as the player has all the permissions required by the command
-        return true;
-      });
+      // Return true, as the player has all the permissions required by the command
+      return true;
+    });
 
     // Return the available commands
     return availableCommands;
@@ -91,6 +86,8 @@ class PlayerCommandExecutorTrait extends PlayerTrait {
 
     // Holds all custom enums that are used in the commands
     packet.dynamicEnums = [];
+    packet.enumValues = [];
+    packet.enums = [];
 
     // Map the commands to the packet property
     packet.commands = [...this.availableCommands].map((command) => {
@@ -107,19 +104,48 @@ class PlayerCommandExecutorTrait extends PlayerTrait {
           const parameters = Object.entries(overload).map(([name, value]) => {
             // Get the parameter constructor by checking if the value is an array.
             const enm = Array.isArray(value) ? value[0] : value;
+            let symbol!: number;
 
-            // Get the name of the parameter.
-            const symbol =
-              enm.type === SoftEnum.type
-                ? (enm as typeof CustomEnum).options.length > 0
-                  ? (0x4_10 << 16) |
+            // Handle custom enums (now strict enums)
+            if (enm.prototype instanceof CustomEnum || enm === CustomEnum) {
+              const options = (enm as typeof CustomEnum).options;
+
+              if (options) {
+                // Create symbol for next enum in the array.
+                const EnumType = 0x300000;
+
+                const enumIndex = packet.enums.length;
+
+                // Combined base symbol and next index.
+                symbol = EnumType | enumIndex;
+
+                // Handle enum values.
+                const values: number[] = [];
+                for (const option of options) {
+                  values.push(packet.enumValues.length);
+                  packet.enumValues.push(option);
+                }
+
+                // Create enum object.
+                packet.enums.push({
+                  name: enm.identifier ?? enm.name,
+                  values: values,
+                });
+              }
+            } else {
+              // Handle soft enums (dynamic enums)
+              symbol =
+                enm.type === SoftEnum.type
+                  ? (enm as typeof SoftEnum).options.length > 0
+                    ? (0x4_10 << 16) |
                     (packet.dynamicEnums.push({
                       name: enm.name,
-                      values: (enm as typeof CustomEnum).options
+                      values: (enm as typeof SoftEnum).options,
                     }) -
                       1)
-                  : enm.symbol
-                : enm.symbol;
+                    : enm.symbol
+                  : enm.symbol;
+            }
 
             // Check if the parameter is optional.
             const optional = Array.isArray(value) ? value[1] : false;
@@ -131,15 +157,15 @@ class PlayerCommandExecutorTrait extends PlayerTrait {
               symbol,
               name,
               optional,
-              options: 0
+              options: 0,
             };
           });
 
           return {
             chaining: false,
-            parameters
+            parameters,
           };
-        })
+        }),
       };
     });
 
@@ -147,8 +173,6 @@ class PlayerCommandExecutorTrait extends PlayerTrait {
     packet.chainedSubcommandValues = [];
     packet.subcommands = [];
     packet.enumConstraints = [];
-    packet.enumValues = [];
-    packet.enums = [];
     packet.postFixes = [];
 
     // Send the packet to the player
