9484e5f5e2a6e8ffe7775a494e84554e47567d9f
Nathan93705 <99233134+Nathan93705@users.noreply.github.com>
2025-09-28 22:35:21 -0700

    Add directory support

--- packages/plugins/src/fileSystem.ts
+++ packages/plugins/src/fileSystem.ts
@@ -1,4 +1,12 @@
-import { existsSync, readFileSync, mkdirSync, writeFileSync } from "fs";
+import {
+  existsSync,
+  readFileSync,
+  mkdirSync,
+  writeFileSync,
+  rmSync,
+  statSync,
+  readdirSync,
+} from "fs";
 import { resolve, sep, isAbsolute } from "path";
 
 import { Logger } from "@serenityjs/logger";
@@ -28,7 +36,10 @@ class PluginFileSystem {
    * @param path The path to validate.
    * @returns True if the path is valid, otherwise false.
    */
-  private validatePath(path: string): boolean {
+  private validatePath(
+    path: string,
+    expectedType?: "file" | "directory"
+  ): boolean {
     // Ensure the path is absolute
     if (!isAbsolute(path)) {
       this.logger.warn(
@@ -45,27 +56,28 @@ class PluginFileSystem {
       return false;
     }
 
+    // If an expected type is provided, ensure the path matches the expected type
+    if (expectedType) {
+      if (!existsSync(path)) {
+        this.logger.warn(
+          `Attempted to access invalid path: ${path}. Path does not exist.`
+        );
+        return false;
+      }
+
+      const stat = statSync(path);
+      if (
+        (expectedType === "file" && !stat.isFile()) ||
+        (expectedType === "directory" && !stat.isDirectory())
+      ) {
+        this.logger.warn(
+          `Attempted to access invalid path: ${path}. Path is not a valid ${expectedType}.`
+        );
+        return false;
+      }
+    }
+
     return true;
-  }
-
-  /**
-   * Checks if a file exists in the plugin's data directory.
-   * @param path The path to the file to check.
-   * @returns True if the file exists, otherwise false.
-   */
-  public hasFile(path: string): boolean {
-    // Check if the plugin data path exists
-    if (!existsSync(this.dataPath)) return false;
-
-    // Resolve the full path to the file
-    const fullPath = resolve(this.dataPath, path);
-
-    // Validate the resolved path
-    const isValid = this.validatePath(fullPath);
-    if (!isValid) return false;
-
-    // Check if the file exists
-    return existsSync(fullPath);
   }
 
   /**
@@ -87,7 +99,8 @@ class PluginFileSystem {
     const fullPath = resolve(this.dataPath, path);
 
     // Validate the resolved path
-    if (!this.validatePath(fullPath)) return null;
+    const isValid = this.validatePath(fullPath, "file");
+    if (!isValid) return null;
 
     // Check if the file exists
     if (!existsSync(fullPath)) return null;
@@ -112,11 +125,131 @@ class PluginFileSystem {
     const fullPath = resolve(this.dataPath, path);
 
     // Validate the resolved path
-    const isValid = this.validatePath(fullPath);
+    const isValid = this.validatePath(fullPath, "file");
     if (!isValid) return;
+
+    // Ensure the directory for the file exists
+    const dirPath = fullPath.substring(0, fullPath.lastIndexOf(sep));
+    if (!existsSync(dirPath)) mkdirSync(dirPath, { recursive: true });
 
     // Write the file data
     return writeFileSync(fullPath, data);
+  }
+
+  /**
+   * Removes a file from the plugin's data directory.
+   * @param path The relative path to the file to remove.
+   */
+  public removeFile(path: string): void {
+    // Check if the plugin data path exists
+    if (!existsSync(this.dataPath)) return;
+
+    // Resolve the full path to the file
+    const fullPath = resolve(this.dataPath, path);
+
+    // Validate the resolved path
+    const isValid = this.validatePath(fullPath, "file");
+    if (!isValid) return;
+
+    // Check if the file exists
+    if (!existsSync(fullPath)) return;
+
+    // Remove the file
+    return rmSync(fullPath);
+  }
+
+  /**
+   * Creates a directory in the plugin's data directory.
+   * @param path The relative path to the directory to create.
+   * @returns The path of the created directory if successful, otherwise undefined.
+   */
+  public mkdir(path: string): string | undefined {
+    // Check if the plugin data path exists
+    if (!existsSync(this.dataPath))
+      // If it doesn't, make the directory
+      mkdirSync(this.dataPath, { recursive: true });
+
+    // Resolve the full path to the directory
+    const fullPath = resolve(this.dataPath, path);
+
+    // Validate the resolved path
+    const isValid = this.validatePath(fullPath);
+    if (!isValid) return;
+
+    // Make the directory
+    return mkdirSync(fullPath, { recursive: true });
+  }
+
+  /**
+   * Removes a directory from the plugin's data directory.
+   * @param path The relative path to the directory to remove.
+   * @param force Whether to forcefully remove the directory and its contents. Defaults to false.
+   */
+  public rmdir(
+    path: string,
+    options: { recursive: boolean; force: boolean } = {
+      recursive: false,
+      force: false,
+    }
+  ): void {
+    // Check if the plugin data path exists
+    if (!existsSync(this.dataPath)) return;
+
+    // Resolve the full path to the directory
+    const fullPath = resolve(this.dataPath, path);
+
+    // Validate the resolved path
+    const isValid = this.validatePath(fullPath, "directory");
+    if (!isValid) return;
+
+    // Check if the directory exists
+    if (!existsSync(fullPath)) return;
+
+    // Remove the directory
+    return rmSync(fullPath, options);
+  }
+
+  /**
+   * Reads the contents of a directory in the plugin's data directory.
+   * @param path The relative path to the directory to read.
+   * @returns An array of file and directory names in the specified directory, or null if the directory does not exist or is invalid.
+   */
+  public readDir(path: string): Array<string> | null {
+    // Ensure the plugin data directory exists
+    if (!existsSync(this.dataPath)) return null;
+
+    // Resolve the full path to the directory
+    const fullPath = resolve(this.dataPath, path);
+
+    // Validate the resolved path
+    const isValid = this.validatePath(fullPath, "directory");
+    if (!isValid) return null;
+
+    // Check if the directory exists
+    if (!existsSync(fullPath)) return null;
+
+    // Read and return the directory contents
+    return readdirSync(fullPath);
+  }
+
+  /**
+   * Checks if a file or directory exists in the plugin's data directory.
+   * @param path The relative path to the file or directory to check.
+   * @returns True if the file or directory exists, otherwise false.
+   */
+  public exists(path: string): boolean {
+    // Ensure the plugin data directory exists
+    if (!existsSync(this.dataPath)) return false;
+
+    // Resolve the full path to the file or directory
+    const fullPath = resolve(this.dataPath, path);
+
+    // Validate the resolved path
+    const isValid = this.validatePath(fullPath);
+    if (!isValid) return false;
+
+    // Check if the file or directory exists
+    return existsSync(fullPath);
   }
 }
 
