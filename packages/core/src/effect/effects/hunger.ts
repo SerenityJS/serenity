43fae1c56f060084356184a96cb821da8d358fc4
Palm <christopher.k.izatt@gmail.com>
2025-10-07 13:08:19 -0400

    Add functionality for hunger effect.

--- packages/core/src/effect/effects/hunger.ts
+++ packages/core/src/effect/effects/hunger.ts
@@ -1,10 +1,21 @@
-import { Color, EffectType } from "@serenityjs/protocol";
+import { Color, EffectType, Gamemode } from "@serenityjs/protocol";
+import { PlayerHungerTrait } from "../../entity";
 
 import { Effect } from "./effect";
+import { Entity } from "../../entity";
 
 class HungerEffect extends Effect {
   public static readonly type: EffectType = EffectType.Hunger;
   public color: Color = new Color(67, 49, 109, 39);
+
+  public onTick?(entity: Entity): void {
+    if (entity.dimension.world.currentTick % 5n !== 0n) return;
+    if (!entity.isPlayer() || entity.getGamemode() == Gamemode.Creative) return;
+    const entityHunger = entity.getTrait(PlayerHungerTrait);
+
+    if (entityHunger.currentValue <= 1) return;
+    entityHunger.exhaustion += (0.025 * (this.amplifier + 1));
+  }
 }
 
 export { HungerEffect };
