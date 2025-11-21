8c5af86be73974aed60b2a9549306263ba8af21b
con <conmaster@noreply.com>
2025-11-21 16:21:49 +0100

    Moved to new authentication system

--- packages/core/src/handlers/login.ts
+++ packages/core/src/handlers/login.ts
@@ -1,3 +1,5 @@
+import { createHash } from "node:crypto";
+
 import {
   ClientData,
   DisconnectReason,
@@ -13,6 +15,10 @@ import {
 } from "@serenityjs/protocol";
 import { Connection } from "@serenityjs/raknet";
 import { createDecoder } from "fast-jwt";
+import {
+  Authentication,
+  AuthenticationType
+} from "@bedrock-apis/carolina-authentication";
 
 import { NetworkHandler } from "../network";
 import { ClientSystemInfo, Player } from "../entity";
@@ -28,127 +34,153 @@ class LoginHandler extends NetworkHandler {
     // Decode the tokens given by the client.
     // This contains the client data, identity data, and public key.
     // Along with the players XUID, display name, and uuid.
-    const { clientData, identityData } = LoginHandler.decode(packet.tokens);
-
-    // Get the clients xuid and username.
-    const xuid = identityData.XUID;
-    const uuid = identityData.identity;
-    const username = identityData.displayName;
-
-    // Check if the xuid is smaller than 16 characters.
-    // If so then the xuid is invalid.
-    // Not sure if this is the best way to check if the xuid is valid, but it works for now.
-    // Possibly add a xuid resolver in the future, but may leave that up to plugins.
-    if (xuid.length < 16) {
-      // Disconnect the player.
-      return this.network.disconnectConnection(
-        connection,
-        "Failed to connect due to having an invalid xuid. Make sure you are connected to Xbox Live before joining the server.",
-        DisconnectReason.InvalidTenant
-      );
-    }
-
-    // Check if the player is already connected.
-    // And if so, disconnect the player the player currently connected.
-    if (this.serenity.getPlayerByXuid(xuid)) {
-      // Get the player to disconnect.
-      const player = this.serenity.getPlayerByXuid(xuid) as Player;
-
-      // Disconnect the player.
-      player.disconnect(
-        "You have been disconnected from the server because you logged in from another location.",
-        DisconnectReason.LoggedInOtherLocation
-      );
-    }
-
-    // Get the default world, and check if it is undefined.
-    // If so, then disconnect the player.
-    const world = this.serenity.getWorld();
-    if (!world)
-      return this.network.disconnectConnection(
-        connection,
-        "There are no worlds registered within the server process.",
-        DisconnectReason.WorldCorruption
-      );
-
-    // Get the default dimension, and check if it is undefined.
-    // If so, then disconnect the player.
-    const dimension = world.getDimension();
-    if (!dimension)
-      return this.network.disconnectConnection(
-        connection,
-        "There are no dimensions registered within the world instance.",
-        DisconnectReason.WorldCorruption
-      );
-
-    // Create a new ClientSystemInfo instance.
-    const clientSystemInfo = new ClientSystemInfo(
-      clientData.DeviceId,
-      clientData.DeviceModel,
-      clientData.DeviceOS,
-      clientData.MaxViewDistance,
-      clientData.MemoryTier
+    const { clientData } = LoginHandler.decode(packet.tokens);
+    const { AuthenticationType: type, Token } = Authentication.parse(
+      packet.tokens.identity
     );
 
-    // Read the player storage data from the world provider.
-    const storage = world.provider.readPlayer(uuid);
-
-    // Get the skin from the client data.
-    const skin = SerializedSkin.from(clientData);
-
-    // Create the properties for the player
-    const properties: Partial<PlayerProperties> = {
-      username,
-      xuid,
-      uuid,
-      clientSystemInfo,
-      skin
-    };
-
-    // Assign the storage if it exists.
-    if (storage) properties.storage = storage;
-
-    // Create a new player instance.
-    const player = new Player(dimension, connection, properties);
-
-    // Set the players xuid and username.
-    this.serenity.players.set(connection, player);
-
-    // Create a new PlayerJoinSignal
-    const signal = new PlayerJoinSignal(player).emit();
-
-    // Check if the signal was cancelled.
-    if (!signal)
-      return player.disconnect(
-        "Failed to join the server.",
-        DisconnectReason.Kicked
+    if (type === AuthenticationType.OfflineSelfSigned)
+      return this.network.disconnectConnection(
+        connection,
+        "Offline mode is not supported. Please connect to xbox services",
+        DisconnectReason.EmptyAuthFromDiscovery
       );
 
-    // TODO: Enable encryption, the public key is given in the tokens
-    // This is with the ClientToSeverHandshake packet & the ServerToClientHandshake packet
-    // But for now, we will just send the player the login status, this will skip the encryption
-    const login = new PlayStatusPacket();
-    login.status = PlayStatus.LoginSuccess;
+    Authentication.authenticate(Token).then(
+      ({ xid, mid: _playFabId, xname }) => {
+        // Get the clients xuid and username.
+        const xuid = xid;
+        const uuid = LoginHandler.getUUIDFromXUID(xuid);
+        const username = xname;
 
-    // Create a new ResourcePacksInfoPacket
-    const resources = new ResourcePacksInfoPacket();
-    resources.mustAccept = this.serenity.resources.properties.mustAccept;
-    resources.hasAddons = false;
-    resources.hasScripts = false;
-    resources.forceDisableVibrantVisuals = false;
-    resources.worldTemplateUuid = "00000000-0000-0000-0000-000000000000";
-    resources.worldTemplateVersion = "";
-    resources.packs = this.serenity.resources.getAllPackDescriptors();
+        // Since new authentication system we know the token is valid, so the xuid is, thats why we shouldn't add own hardcoded validations
+        // // Check if the xuid is smaller than 16 characters.
+        // // If so then the xuid is invalid.
+        // // Not sure if this is the best way to check if the xuid is valid, but it works for now.
+        // // Possibly add a xuid resolver in the future, but may leave that up to plugins.
+        // if (xuid.length < 16) {
+        //   // Disconnect the player.
+        //   return this.network.disconnectConnection(
+        //     connection,
+        //     "Failed to connect due to having an invalid xuid. Make sure you are connected to Xbox Live before joining the server.",
+        //     DisconnectReason.InvalidTenant
+        //   );
+        // }
 
-    // Log the join event to the console
-    world.logger.info(
-      `§8[§9${player.username}§8] Event:§r Player joined the server.`
+        // Check if the player is already connected.
+        // And if so, disconnect the player the player currently connected.
+        if (this.serenity.getPlayerByXuid(xuid)) {
+          // Get the player to disconnect.
+          const player = this.serenity.getPlayerByXuid(xuid) as Player;
+
+          // Disconnect the player.
+          player.disconnect(
+            "You have been disconnected from the server because you logged in from another location.",
+            DisconnectReason.LoggedInOtherLocation
+          );
+        }
+
+        // Get the default world, and check if it is undefined.
+        // If so, then disconnect the player.
+        const world = this.serenity.getWorld();
+        if (!world)
+          return this.network.disconnectConnection(
+            connection,
+            "There are no worlds registered within the server process.",
+            DisconnectReason.WorldCorruption
+          );
+
+        // Get the default dimension, and check if it is undefined.
+        // If so, then disconnect the player.
+        const dimension = world.getDimension();
+        if (!dimension)
+          return this.network.disconnectConnection(
+            connection,
+            "There are no dimensions registered within the world instance.",
+            DisconnectReason.WorldCorruption
+          );
+
+        // Create a new ClientSystemInfo instance.
+        const clientSystemInfo = new ClientSystemInfo(
+          clientData.DeviceId,
+          clientData.DeviceModel,
+          clientData.DeviceOS,
+          clientData.MaxViewDistance,
+          clientData.MemoryTier
+        );
+
+        // Read the player storage data from the world provider.
+        const storage = world.provider.readPlayer(uuid);
+
+        // Get the skin from the client data.
+        const skin = SerializedSkin.from(clientData);
+
+        // Create the properties for the player
+        const properties: Partial<PlayerProperties> = {
+          username,
+          xuid,
+          uuid,
+          clientSystemInfo,
+          skin
+        };
+
+        // Assign the storage if it exists.
+        if (storage) properties.storage = storage;
+
+        // Create a new player instance.
+        const player = new Player(dimension, connection, properties);
+
+        // Set the players xuid and username.
+        this.serenity.players.set(connection, player);
+
+        // Create a new PlayerJoinSignal
+        const signal = new PlayerJoinSignal(player).emit();
+
+        // Check if the signal was cancelled.
+        if (!signal)
+          return void player.disconnect(
+            "Failed to join the server.",
+            DisconnectReason.Kicked
+          );
+
+        // TODO: Enable encryption, the public key is given in the tokens
+        // This is with the ClientToSeverHandshake packet & the ServerToClientHandshake packet
+        // But for now, we will just send the player the login status, this will skip the encryption
+        const login = new PlayStatusPacket();
+        login.status = PlayStatus.LoginSuccess;
+
+        // Create a new ResourcePacksInfoPacket
+        const resources = new ResourcePacksInfoPacket();
+        resources.mustAccept = this.serenity.resources.properties.mustAccept;
+        resources.hasAddons = false;
+        resources.hasScripts = false;
+        resources.forceDisableVibrantVisuals = false;
+        resources.worldTemplateUuid = "00000000-0000-0000-0000-000000000000";
+        resources.worldTemplateVersion = "";
+        resources.packs = this.serenity.resources.getAllPackDescriptors();
+
+        // Log the join event to the console
+        world.logger.info(
+          `§8[§9${player.username}§8] Event:§r Player joined the server.`
+        );
+
+        // Send the player the login status packet and the resource pack info packet.
+        player.send(login, resources);
+        return null;
+      },
+      () =>
+        this.network.disconnectConnection(
+          connection,
+          "Your client is not authenticated or your authentication token expired.",
+          DisconnectReason.NotAuthenticated
+        )
     );
-
-    // Send the player the login status packet and the resource pack info packet.
-    player.send(login, resources);
   }
-
+  public static getUUIDFromXUID(xuid: string): string {
+    return uuidFromBytes(
+      new TextEncoder().encode(`pocket-auth-1-xuid:${xuid}`)
+    );
+  }
   /**
    * Decodes the login tokens and returns the data
    * @param tokens The login tokens
@@ -186,4 +218,10 @@ class LoginHandler extends NetworkHandler {
   }
 }
 
+function uuidFromBytes(input: Uint8Array): string {
+  const bytes = createHash("md5").update(input).digest();
+  bytes[6] = (bytes[6]! & 0x0f) | 0x30; // version 3
+  bytes[8] = (bytes[8]! & 0x3f) | 0x80; // RFC 4122 variant
+  return `${bytes.subarray(0, 4).toHex()}-${bytes.subarray(4, 6).toHex()}-${bytes.subarray(6, 8).toHex()}-${bytes.subarray(8, 10).toHex()}-${bytes.subarray(10, 16).toHex()}`;
+}
 export { LoginHandler };
