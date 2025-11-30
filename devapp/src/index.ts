92fbffd8b44ec895d019fc8b8b6e5b0134e7e971
Payton Kerby <paytonmk744@outlook.com>
2025-11-30 10:32:38 -0600

    chore: remove test code

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,9 +1,4 @@
-import {
-  Serenity,
-  LevelDBProvider,
-  WorldEvent,
-  ActionForm
-} from "@serenityjs/core";
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
 // Create a new Serenity instance
@@ -23,16 +18,3 @@ serenity.registerProvider(LevelDBProvider, { path: "./worlds" });
 
 // Start the server
 serenity.start();
-
-serenity.after(WorldEvent.EntitySpawned, async ({ entity }) => {
-  if (!entity.isPlayer()) return;
-
-  const form = new ActionForm("Test Form", "This is a test action form.");
-  form.header("Welcome Player");
-  form.divider();
-  form.button("Click Me", { type: "path", data: "textures/ui/icon_book" });
-  form.label("This is a label element.");
-  const result = await form.show(entity);
-
-  console.log(result);
-});
