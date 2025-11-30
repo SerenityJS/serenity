3e6d0602aab1b8caca9537d3ef51a1cad564493c
Payton Kerby <paytonmk744@outlook.com>
2025-11-30 10:31:09 -0600

    feat: added `header`, `label`, and `divider` to `ActionForm`

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,4 +1,9 @@
-import { Serenity, LevelDBProvider } from "@serenityjs/core";
+import {
+  Serenity,
+  LevelDBProvider,
+  WorldEvent,
+  ActionForm
+} from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
 
 // Create a new Serenity instance
@@ -18,3 +23,16 @@ serenity.registerProvider(LevelDBProvider, { path: "./worlds" });
 
 // Start the server
 serenity.start();
+
+serenity.after(WorldEvent.EntitySpawned, async ({ entity }) => {
+  if (!entity.isPlayer()) return;
+
+  const form = new ActionForm("Test Form", "This is a test action form.");
+  form.header("Welcome Player");
+  form.divider();
+  form.button("Click Me", { type: "path", data: "textures/ui/icon_book" });
+  form.label("This is a label element.");
+  const result = await form.show(entity);
+
+  console.log(result);
+});
