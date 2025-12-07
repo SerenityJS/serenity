61e2e920c67d81434684a66ae3772b6cb3936632
con <conmaster@noreply.com>
2025-12-07 12:49:26 +0100

    Improved Polify Performance & Fixed missing polify for XUID -> UUID

--- packages/core/src/polyfill/uint8-array.ts
+++ packages/core/src/polyfill/uint8-array.ts
@@ -1,7 +1,12 @@
 // NodeJS backward compatibility for Uint8Array.fromBase64
 if (!Uint8Array.fromBase64) {
   // Extend Uint8Array with fromBase64 method
-  Uint8Array.fromBase64 = function (base64String: string): Uint8Array {
+  Uint8Array.fromBase64 = function (
+    base64String: string,
+    options?: { alphabet?: "base64" | "base64url" }
+  ): Uint8Array {
+    return Buffer.from(base64String, options?.alphabet ?? "base64");
+    /*
     const binaryString = atob(base64String);
     const len = binaryString.length;
     const bytes = new Uint8Array(len);
@@ -10,6 +15,12 @@ if (!Uint8Array.fromBase64) {
       bytes[i] = binaryString.charCodeAt(i);
     }
 
-    return bytes;
+    return bytes;*/
+  };
+}
+
+if (!Uint8Array.prototype.toHex) {
+  Uint8Array.prototype.toHex = function toHex(this: Uint8Array) {
+    return Buffer.prototype.toString.call(this, "hex");
   };
 }
