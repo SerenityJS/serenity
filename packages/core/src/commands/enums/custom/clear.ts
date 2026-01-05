6c741621dadf752fa61dc831b6b4f21c774e5503
Palm <christopher.k.izatt@gmail.com>
2026-01-05 17:31:51 -0500

    Implement vanilla effect command
    - Implement a new `/effect` command on parity with vanilla.

---
+++ packages/core/src/commands/enums/custom/clear.ts
@@ -0,0 +1,8 @@
+import { CustomEnum } from "./custom";
+
+class ClearEnum extends CustomEnum {
+  public static readonly identifier = "clear";
+  public static readonly options = ["clear"];
+}
+
+export { ClearEnum };
