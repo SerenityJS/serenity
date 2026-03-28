b794067545e6c75c64f79afaaa6b5cf04e07b40f
Payton Kerby <paytonmk744@outlook.com>
2026-03-28 15:31:13 -0500

    chore: remove testing code

--- devapp/src/index.ts
+++ devapp/src/index.ts
@@ -1,14 +1,5 @@
-import {
-  Serenity,
-  LevelDBProvider,
-  Observable,
-  WorldEvent,
-  Player,
-  CustomForm
-} from "@serenityjs/core";
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
 import { Pipeline } from "@serenityjs/plugins";
-
-const test = new Observable<string>("test");
 
 // Create a new Serenity instance
 const serenity = new Serenity({
@@ -18,31 +9,6 @@ const serenity = new Serenity({
     debugLogging: true
   }
 });
-
-serenity.on(WorldEvent.WorldInitialize, ({ world }) => {
-  world.commandPalette.register("test", "", ({ origin }) => {
-    if (!(origin instanceof Player)) return;
-
-    const form = new CustomForm(test).closeButton();
-    form.divider();
-    form.spacer();
-
-    form.button("Click Me!", (player) =>
-      player.onScreenDisplay.setToast("DDUI", "Button clicked!")
-    );
-
-    form.spacer();
-    form.divider();
-
-    form.label("Label Test");
-
-    form.show(origin);
-  });
-});
-
-serenity.on(WorldEvent.WorldTick, ({ currentTick }) =>
-  test.setValue("Current tick: " + currentTick.toString())
-);
 
 // Create a new plugin pipeline
 new Pipeline(serenity, { path: "./plugins" });
