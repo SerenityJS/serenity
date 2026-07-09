6681889e9224c3682d5ef16ff7c06128a1a9973a
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-07-09 23:53:35 +0300

    Use new String enum for LevelSoundEventPacket

--- packages/protocol/src/proto/data/level-sound-event.ts
+++ packages/protocol/src/proto/data/level-sound-event.ts
@@ -2,20 +2,22 @@ import {
   Bool,
   Endianness,
   Int64,
-  VarInt,
   VarString,
   ZigZag
 } from "@serenityjs/binarystream";
 import { Proto, Serialize } from "@serenityjs/raknet";
 
-import { type LevelSoundEvent, Packet } from "../../enums";
+import { Packet } from "../../enums";
 import { Vector3f } from "../types";
 
 import { DataPacket } from "./data-packet";
 
 @Proto(Packet.LevelSoundEvent)
 class LevelSoundEventPacket extends DataPacket {
-  @Serialize(VarInt) public event!: LevelSoundEvent;
+  /**
+   * As of 1.26.30 r/26.3 LevelEvent is a string
+   */
+  @Serialize(VarString) public event!: string;
   @Serialize(Vector3f) public position!: Vector3f;
   @Serialize(ZigZag) public data!: number;
   @Serialize(VarString) public actorIdentifier!: string;
