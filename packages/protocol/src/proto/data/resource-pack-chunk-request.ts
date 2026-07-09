0416cf19f8f0cbbd5875280ec2131f93832a4fea
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-07-10 00:00:16 +0300

    chunkId is now i32!?

--- packages/protocol/src/proto/data/resource-pack-chunk-request.ts
+++ packages/protocol/src/proto/data/resource-pack-chunk-request.ts
@@ -1,4 +1,4 @@
-import { Endianness, Uint32 } from "@serenityjs/binarystream";
+import { Endianness, Int32 } from "@serenityjs/binarystream";
 import { Proto, Serialize } from "@serenityjs/raknet";
 
 import { Packet } from "../../enums";
@@ -9,7 +9,7 @@ import { DataPacket } from "./data-packet";
 @Proto(Packet.ResourcePackChunkRequest)
 class ResourcePackChunkRequestPacket extends DataPacket {
   @Serialize(RequestedResourcePack) public pack!: RequestedResourcePack;
-  @Serialize(Uint32, { endian: Endianness.Little }) public chunkId!: number;
+  @Serialize(Int32, { endian: Endianness.Little }) public chunkId!: number;
 }
 
 export { ResourcePackChunkRequestPacket };
