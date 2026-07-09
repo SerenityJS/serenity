096a2901f49cc4c73a2c86ccb23499c2c6c9338c
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-07-09 23:35:47 +0300

    Fix name according to mojang

--- packages/protocol/src/enums/boss-event-color.ts
+++ packages/protocol/src/enums/boss-event-color.ts
@@ -1,12 +1,19 @@
 enum BossEventColor {
-  Pink,
-  Blue,
-  Red,
-  Green,
-  Yellow,
-  Magenta,
-  Purple,
-  White
+  Pink = 0,
+  Blue = 1,
+  Red = 2,
+  Green = 3,
+  Yellow = 4,
+
+  Purple = 5,
+
+  /**
+   * RebeccaPurple is what mojang calls Magenta i guess
+   * @link https://github.com/Mojang/bedrock-protocol-docs/blob/r/26_u3/json/BossEventPacket.json
+   */
+  RebeccaPurple = 6,
+
+  White = 7,
 }
 
 export { BossEventColor };
